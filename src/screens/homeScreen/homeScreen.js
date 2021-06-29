7ac07c2458e0f2dca742a23fb0e3a028b9dfbaf2
Malinda <nmalinda22@gmail.com>
2021-06-29 19:17:03 +0530

    Add text field component

--- src/screens/homeScreen/homeScreen.js
+++ src/screens/homeScreen/homeScreen.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef } from 'react';
 import { StyleSheet, Text, View,StatusBar, SafeAreaView } from 'react-native';
 import { colors, size } from '../../theme'
 import { Button, Header, ReminderButton  } from '../../components'
+import InputField from '../../layout/forms/textField'
 // Connect redux store.
 import { useSelector } from 'react-redux';
 ;
@@ -28,6 +29,40 @@ const HomeScreen = props => {
       <Button title="Solid Button" color={colors.RED} />
       <Button title="Solid Button" color={colors.YELLOW} custonTextStyle={{color: colors.WHITE}} />
       <Button title="Solid Button" color={colors.YELLOW} custonTextStyle={{color: colors.GREEN, fontSize:9}} />
+
+      <InputField
+        inputBackgroundColor={colors.DARK_BLUE} 
+        borderColor={colors.GREEN}
+        inputColor={colors.GREEN}
+        placeholderColor={colors.GREEN}
+        placeholder={'Enter name here'}
+        inputTextAlign={'center'}
+      />
+
+<View style={{backgroundColor: colors.RED, paddingTop: 20}}>
+
+      <InputField
+        inputBackgroundColor={colors.RED} 
+        borderColor={colors.WHITE}
+        inputColor={colors.WHITE}
+        placeholderColor={colors.WHITE}
+        placeholder={'Enter pet name'}
+        inputTextAlign={'center'}
+      />
+
+      </View>
+
+      <View style={{backgroundColor: colors.GREEN, paddingTop: 20}}>
+
+      <InputField
+        inputBackgroundColor={colors.WHITE} 
+        borderColor={colors.WHITE}
+        inputColor={colors.GREEN}
+        placeholderColor={colors.GREEN}
+        placeholder={'Enter card name'}
+        inputTextAlign={'left'}
+      />
+      </View>
 
     </SafeAreaView>
   );
