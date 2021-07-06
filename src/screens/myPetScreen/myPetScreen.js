50d33bca1048fd8ea704799955b7733362b3d106
Dinusha <dinusha@nuclei.tech>
2021-07-06 09:27:51 +0530

    add middle header

--- src/screens/myPetScreen/myPetScreen.js
+++ src/screens/myPetScreen/myPetScreen.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import { Text,SafeAreaView } from 'react-native';
-import { styles } from './styles';
-
+import React, { useEffect, useState, useRef } from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { Button, Header, ReminderButton, Layout2 } from '../../components'
 // Connect redux store.
 import { useSelector } from 'react-redux';
-
+;
+import { navigate } from '../../navigation/navigation';
 
 const MyPetScreen = props => {
   const { theme } = useSelector(state => state.theme);
+  const action = () => {
+    console.log('in');
+  }
 
   return (
-        <SafeAreaView>
-            <Text>MyPet Screen</Text>
-        </SafeAreaView>
+    <SafeAreaView style={styles.container}>
+      <Layout2
+        type={'small'}
+        layoutColor={theme.Theme.colors.RED} // color is required
+      >
+        <Header title={'pet harmony'} noIcon customContainerStyle={{justifyContent:'center'}}/>
+        <Text>From parent</Text>
+      </Layout2>
+    </SafeAreaView>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+});
 
 
 export default MyPetScreen;
